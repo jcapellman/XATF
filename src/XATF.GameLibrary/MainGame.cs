ca183ef327a26fd4e1f6a783095522653e006bde
Jarred Capellman <jcapellman@hotmail.com>
2018-04-30 20:19:59 -0500

    Added: Initial commit of the GameLibrary

---
+++ src/XATF.GameLibrary/MainGame.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XATF.GameLibrary
+{
+    public class MainGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public MainGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+        
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+        
+        protected override void UnloadContent()
+        {
+
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+        
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+            base.Draw(gameTime);
+        }
+    }
+}
