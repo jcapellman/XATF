0e700ff87c77772b2a7428d299bb8c8feab54f58
Jarred Capellman <jcapellman@hotmail.com>
2018-04-30 21:24:54 -0500

    Added: Initial work on the Renderable class

---
+++ src/XATF.GameLibrary/Renderables/Renderable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XATF.GameLibrary.Renderables
+{
+    public abstract class Renderable
+    {
+        protected Texture2D Texture;
+        protected Vector2 Position;
+
+        protected abstract string FolderBase { get; }
+
+        public void Initialize(string textureName, Vector2 position, ContentManager content)
+        {
+            Texture = content.Load<Texture2D>($"{FolderBase}\\{textureName}");
+            Position = position;
+        }
+
+        public void Render(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.Black);
+        }
+    }
+}
