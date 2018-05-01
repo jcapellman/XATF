dc0a99d40054dc7ca342a627c437dfbb89d05e38
Jarred Capellman <jcapellman@hotmail.com>
2018-04-30 20:19:11 -0500

    Change: Cleaned up the unused using statements in the GamePage.xaml.cs

--- src/XATF.UWP/Properties/AssemblyInfo.cs
+++ src/XATF.UWP/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("XATF.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("XATF.UWP")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyCopyright("Copyright © Jarred Capellman")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
