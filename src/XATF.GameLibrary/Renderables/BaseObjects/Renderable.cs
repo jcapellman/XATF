54a2310c322a98de7977376f644956fb7feb445a
Jarred Capellman <jcapellman@hotmail.com>
2018-05-05 07:51:22 -0500

    Added: RenderableBaseType logic to support Texture and Font

--- src/XATF.GameLibrary/Renderables/BaseObjects/Renderable.cs
+++ src/XATF.GameLibrary/Renderables/BaseObjects/Renderable.cs
@@ -2,18 +2,30 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
+using XATF.GameLibrary.Enums;
+
 namespace XATF.GameLibrary.Renderables.BaseObjects
 {
     public abstract class Renderable
     {
         protected Texture2D Texture;
+        protected SpriteFont Font;
         protected Vector2 Position;
 
         protected abstract string FolderBase { get; }
 
-        protected Renderable(string textureName, Vector2 position, ContentManager content)
+        protected Renderable(string textureName, RenderableBaseType renderableBaseType, Vector2 position, ContentManager content)
         {
-            Texture = content.Load<Texture2D>($"{FolderBase}\\{textureName}");
+            switch (renderableBaseType)
+            {
+                case RenderableBaseType.TEXTURE:
+                    Texture = content.Load<Texture2D>($"{FolderBase}\\{textureName}");
+                    break;
+                case RenderableBaseType.FONT:
+                    Font = content.Load<SpriteFont>(textureName);
+                    break;
+            }
+
             Position = position;
         }
 
