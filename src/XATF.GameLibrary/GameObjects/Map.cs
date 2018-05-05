67c1e86321cb1b2be49033d0045741bb2d64f1c3
Jarred Capellman <jcapellman@hotmail.com>
2018-05-05 07:42:04 -0500

    Change: Map class refactoring to utilize the BaseGameObject methods

--- src/XATF.GameLibrary/GameObjects/Map.cs
+++ src/XATF.GameLibrary/GameObjects/Map.cs
@@ -1,63 +1,29 @@
-﻿using System.Collections.Generic;
-
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
 
 using XATF.GameLibrary.Renderables.BaseObjects;
 
 namespace XATF.GameLibrary.GameObjects
 {
-    public class Map
+    public class Map : BaseGameObject<MapTile>
     {
-        private List<Aircraft> _enemies = new List<Aircraft>();
-        private readonly List<MapTile> _mapTiles = new List<MapTile>();
-
-        public void LoadMap(string name, ContentManager content)
+        public override void Initialize(string objectName, ContentManager content)
         {
-            var aircraft = new Aircraft();
-
-            aircraft.Initialize("Mig51", new Vector2(30, -10), content);
-
-            _enemies.Add(aircraft);
-            _enemies.Add(aircraft);
-
+            // TODO: Load actual map file from the objectName parameter
             for (var x = 0; x <= 3; x++)
             {
                 for (var y = 0; y < 100; y++)
                 {
-                    var tile = new MapTile();
-
-                    tile.Initialize("Water", new Vector2(x * 256, -y * 256), content);
-
-                    _mapTiles.Add(tile);
+                    Renderables.Add(new MapTile("Water", new Vector2(x * 256, -y * 256), content));
                 }
             }
         }
 
-        public void Update()
+        public override void Update(int x, int y)
         {
-            foreach (var tile in _mapTiles)
+            foreach (var tile in Renderables)
             {
                 tile.Update(0, 2);
-            }
-
-            foreach (var enemy in _enemies)
-            {
-                enemy.Update(0, 3);
-            }
-        }
-
-        public void Render(SpriteBatch spriteBatch)
-        {
-            foreach (var tile in _mapTiles)
-            {
-                tile.Render(spriteBatch);
-            }
-
-            foreach (var enemy in _enemies)
-            {
-                enemy.Render(spriteBatch);
             }
         }
     }
