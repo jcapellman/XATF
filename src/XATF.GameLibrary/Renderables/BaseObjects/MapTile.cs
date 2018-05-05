26f5f5e8d0b91d763ec1c062039f8b5f0faada7e
Jarred Capellman <jcapellman@hotmail.com>
2018-05-05 07:40:24 -0500

    Change: Implemented the MapTile constructor

--- src/XATF.GameLibrary/Renderables/BaseObjects/MapTile.cs
+++ src/XATF.GameLibrary/Renderables/BaseObjects/MapTile.cs
@@ -1,7 +1,14 @@
-﻿namespace XATF.GameLibrary.Renderables.BaseObjects
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace XATF.GameLibrary.Renderables.BaseObjects
 {
     public class MapTile : Renderable
     {
         protected override string FolderBase => "Tiles";
+
+        public MapTile(string textureName, Vector2 position, ContentManager content) : base(textureName, position, content)
+        {
+        }
     }
 }
