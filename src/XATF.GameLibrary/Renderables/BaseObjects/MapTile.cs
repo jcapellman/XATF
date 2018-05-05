f23fabc6944dddc145722e2515c0056dc010e456
Jarred Capellman <jcapellman@hotmail.com>
2018-05-05 07:52:01 -0500

    Added: RenderableBaseType argument to the MapTile class

--- src/XATF.GameLibrary/Renderables/BaseObjects/MapTile.cs
+++ src/XATF.GameLibrary/Renderables/BaseObjects/MapTile.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 
+using XATF.GameLibrary.Enums;
+
 namespace XATF.GameLibrary.Renderables.BaseObjects
 {
     public class MapTile : Renderable
     {
         protected override string FolderBase => "Tiles";
 
-        public MapTile(string textureName, Vector2 position, ContentManager content) : base(textureName, position, content)
+        public MapTile(string textureName, Vector2 position, ContentManager content) : base(textureName, RenderableBaseType.TEXTURE, position, content)
         {
         }
     }
