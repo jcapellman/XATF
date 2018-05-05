fccfb0fd78b82eafb2a2d903768e5a4d11160983
Jarred Capellman <jcapellman@hotmail.com>
2018-05-05 07:40:54 -0500

    Change: Implemented the Aircraft Constructor

--- src/XATF.GameLibrary/Renderables/BaseObjects/Aircraft.cs
+++ src/XATF.GameLibrary/Renderables/BaseObjects/Aircraft.cs
@@ -1,7 +1,14 @@
-﻿namespace XATF.GameLibrary.Renderables.BaseObjects
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace XATF.GameLibrary.Renderables.BaseObjects
 {
     public class Aircraft : Renderable
     {
         protected override string FolderBase => "Aircraft";
+
+        public Aircraft(string textureName, Vector2 position, ContentManager content) : base(textureName, position, content)
+        {
+        }
     }
 }
