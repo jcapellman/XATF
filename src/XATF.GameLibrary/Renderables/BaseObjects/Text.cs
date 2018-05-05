8bdb39b76250628d967e0ee690a4c1f031a5fee9
Jarred Capellman <jcapellman@hotmail.com>
2018-05-05 07:50:46 -0500

    Change: Refactored the Text class to use the new Renderable class functionality

--- src/XATF.GameLibrary/Renderables/BaseObjects/Text.cs
+++ src/XATF.GameLibrary/Renderables/BaseObjects/Text.cs
@@ -2,26 +2,24 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
+using XATF.GameLibrary.Enums;
+
 namespace XATF.GameLibrary.Renderables.BaseObjects
 {
     public class Text : Renderable
     {
-        private readonly SpriteFont _font;
-
         private readonly string _stringContent;
 
         protected override string FolderBase => "Fonts";
         
         public override void Render(SpriteBatch spriteBatch)
         {
-            spriteBatch.DrawString(_font, _stringContent, new Vector2(10, 20), Color.White);
+            spriteBatch.DrawString(Font, _stringContent, new Vector2(10, 20), Color.White);
         }
 
-        public Text(string textureName, Vector2 position, ContentManager content) : base(textureName, position, content)
+        public Text(string stringContent, string fontName, Vector2 position, ContentManager content) : base(fontName, RenderableBaseType.FONT, position, content)
         {
-            _font = content.Load<SpriteFont>("HUD");
-
-            _stringContent = textureName;
+            _stringContent = stringContent;
         }
     }
 }
