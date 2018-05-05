a099da745421f6591deac5c1251026cae5e2a98a
Jarred Capellman <jcapellman@hotmail.com>
2018-05-05 05:51:46 -0500

    Added: Initial work on the BaseGameObject Class for Game Objects

---
+++ src/XATF.GameLibrary/GameObjects/BaseGameObject.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace XATF.GameLibrary.GameObjects
+{
+    public abstract class BaseGameObject
+    {
+        public abstract void Render(SpriteBatch spriteBatch);
+    }
+}
