be3c03e9668a3a5b71e1df05392a36d6ab11f3da
Jarred Capellman <jcapellman@hotmail.com>
2018-05-05 07:36:42 -0500

    Added: Initial commit of the Enemy BaseGameObject

---
+++ src/XATF.GameLibrary/GameObjects/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+using XATF.GameLibrary.Renderables.BaseObjects;
+
+namespace XATF.GameLibrary.GameObjects
+{
+    public class Enemy : BaseGameObject<Aircraft>
+    {
+        public override void Initialize(string objectName, ContentManager content)
+        {
+            Renderable = new Aircraft(objectName, Vector2.One, content);
+        }
+
+        public override void Update(int x, int y)
+        {
+            
+        }
+    }
+}
