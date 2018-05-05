4a25ec9848fce69c5a5a69b0fa5ea28215076a06
Jarred Capellman <jcapellman@hotmail.com>
2018-05-05 07:52:18 -0500

    Added: RenderableBaseType argument to the Aircraft class

--- src/XATF.GameLibrary/Renderables/BaseObjects/Aircraft.cs
+++ src/XATF.GameLibrary/Renderables/BaseObjects/Aircraft.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 
+using XATF.GameLibrary.Enums;
+
 namespace XATF.GameLibrary.Renderables.BaseObjects
 {
     public class Aircraft : Renderable
     {
         protected override string FolderBase => "Aircraft";
 
-        public Aircraft(string textureName, Vector2 position, ContentManager content) : base(textureName, position, content)
+        public Aircraft(string textureName, Vector2 position, ContentManager content) : base(textureName, RenderableBaseType.TEXTURE, position, content)
         {
         }
     }
