2394f2b3d4dc12d6762478492e1b317507c726ef
Jarred Capellman <jcapellman@hotmail.com>
2018-05-13 09:56:58 -0500

    Added: Initial work on the StaticGraphic and MainMenuScene

---
+++ src/XATF.GameLibrary/Renderables/BaseObjects/StaticGraphic.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+using XATF.GameLibrary.Enums;
+
+namespace XATF.GameLibrary.Renderables.BaseObjects
+{
+    public class StaticGraphic : Renderable
+    {
+        public StaticGraphic(string textureName, Vector2 position, ContentManager content) : base(textureName, RenderableBaseType.TEXTURE, position, content)
+        {
+        }
+
+        protected override string FolderBase => "Static";
+    }
+}
---
+++ src/XATF.GameLibrary/Scenes/MainMenuScene.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using XATF.GameLibrary.Renderables.BaseObjects;
+
+namespace XATF.GameLibrary.Scenes
+{
+    public class MainMenuScene : BaseScene
+    {
+        private StaticGraphic background;
+        private StaticGraphic selector;
+
+        public override void Initialize(ContentManager content, string argument = null)
+        {
+            background = new StaticGraphic("MainMenuBG", Vector2.One, content);
+            selector = new StaticGraphic("Selector", Vector2.One, content);
+        }
+
+        public override void Update()
+        {
+            var keys = GetKeyDown();
+
+            foreach (var key in keys)
+            {
+                switch (key)
+                {
+                    case Keys.Escape:
+                        OnQuitGame();
+                        break;
+                    case Keys.Up:
+                    case Keys.Down:
+                    case Keys.Left:
+                    case Keys.Right:
+                        // Handle selector movement
+                        break;                    
+                }
+            }
+        }
+
+        public override void Render(SpriteBatch spriteBatch)
+        {
+            
+        }
+    }
+}
